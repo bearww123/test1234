d0efddc6c983e886642912bfb53060a21e1fd1d1
choi_Bar <144893447+bearww123@users.noreply.github.com>
2024-06-24 14:26:04 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-# from crawling_class import stock_craw
+from crawling_class import stock_craw
 
 def main_page():
     st.set_page_config(page_title='Stock_predict main', layout='wide')
@@ -13,8 +13,8 @@ def main_page():
     st.write('더 많은 종목 정보를 검색하세요 !')
     search_query = st.text_input('검색어 입력')
 
-    # if st.button('검색'):
-    #     stock_craw.search_craw(search_query)
+    if st.button('검색'):
+        stock_craw.search_craw(search_query)
 
 if __name__ == '__main__':
     main_page()
